8a1a68a87f966fd2f429870fe81eb80207700864
festwirt <darkwirt@gmail.com>
2018-03-27 14:21:53 +0200

    Layout BulldogDetails geändert

--- webapp/controller/ahuberController/BulldogDetails.controller.js
+++ webapp/controller/ahuberController/BulldogDetails.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("rab.view.ahuberView.controller.BulldogDetails", {
+	return Controller.extend("rab.controller.ahuberController.BulldogDetails", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
