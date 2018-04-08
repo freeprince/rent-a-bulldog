d924b1d3b3728c1be0ebdd830f9db92cb0cb3976
Alexander Gabel <alexgabel14@gmail.com>
2018-04-08 13:33:40 +0200

    prototype für neue listendarstellung

---
+++ webapp/control/BulldogListItem.js
@@ -0,0 +1,25 @@
+// https://blogs.sap.com/2017/03/25/ui5-custom-controls-with-aggregation/
+sap.ui.define("rab.control.BulldogListItem", [
+	"sap/ui/core/Control"
+], function(Control, ) {
+	"use strict";
+
+	return Control.extend("rab.control.BulldogListItem", {
+		metadata: {
+			properties: {
+				
+			},
+
+			aggregations: {
+				
+			},
+
+			events: {
+				
+			}
+		},
+		
+		init: function() {
+        }
+    });
+});
